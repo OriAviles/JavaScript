a6efb6ce7206ba7c7f13aa0fea0b846dfcfd2724
OriAviles <oriaviles2@gmail.com>
2022-05-30 23:52:37 -0300

    fetch

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-class Producto {
+/* class Producto {
     constructor(id, nombre, precio) {
         this.id = id;
         this.nombre = nombre;
@@ -10,17 +10,22 @@ const producto1 = new Producto(1, "Pop It", 195);
 const producto2 = new Producto(2, "Spinner", 80);
 const producto3 = new Producto(3, "Slime", 120);
 const producto4 = new Producto(4, "Squishy", 245);
-
 let productos = [producto1, producto2, producto3, producto4]
+*/
 
 let carrito = JSON.parse(localStorage.getItem("productos")) ?? []
 
 let divProductos = document.getElementById('productos')
 
-    productos.forEach(producto => {
+    fetch("./datos.json")
+    .then((async function (resp){
+        const data = await resp.json();
+        productos = [...data];
+        productos.forEach(producto => {
         divProductos.innerHTML += `
-            <div class="card" id="producto${producto.id}" style="width: 25rem;">
+            <div class="card" id="producto${producto.id}" style="width: 80rem;">
                 <div class="card-body">
+                <img class="imgProductos" src=${producto.imagen} style="width: 10rem;" >
                     <h5 class="card-title">${producto.nombre}</h5>
                     <p class="card-text">Precio:$${producto.precio}</p>
                     <button id="boton${producto.id}" class="btn btn-light">Agregar al carrito</button>
@@ -46,4 +51,4 @@ let divProductos = document.getElementById('productos')
         })
     })
 
-    
+}))
